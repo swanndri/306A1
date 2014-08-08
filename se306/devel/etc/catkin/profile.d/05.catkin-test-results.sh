b86b9a8c5ece5b50a2fd410baa464a881ebaaaa0
Milos Ivanovic <milos@softeng306.vm.bhd-akl1.xsrv.net.nz>
2014-08-08 20:58:53 +1200

    catkin+rospy+pubsub

---
+++ se306/devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/milos/306A1/se306/build/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
