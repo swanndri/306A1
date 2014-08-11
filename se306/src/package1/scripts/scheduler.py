4bee4d7ad4436f9f52531c0ef5ea0022af747343
Simon du Preez <smdupnz@yahoo.co.nz>
2014-08-12 01:06:34 +1200

    added library so file doesnt just fail

--- se306/src/package1/scripts/scheduler.py
+++ se306/src/package1/scripts/scheduler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import roslib
 import rospy
 
 def shutdown():
@@ -7,5 +8,5 @@ def shutdown():
 
 roslib.load_manifest('package1')
 rospy.init_node('node1', anonymous=False)
-
+rospy.on_shutdown(shutdown)
 
