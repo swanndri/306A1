7ebc6909c4ceb7c07a5fe156bc9917d5ae17bbe6
Ben Brown <benbrown93@gmail.com>
2014-08-12 23:47:52 +1200

    Updated my world file to enable lasers, give walls height. Added subs to node1.py

--- se306/src/package1/scripts/node1.py
+++ se306/src/package1/scripts/node1.py
@@ -5,6 +5,7 @@ import rospy
 import geometry_msgs.msg
 import std_msgs.msg
 import nav_msgs.msg
+import sensor_msgs.msg
 import math
 import tf
 
@@ -18,19 +19,19 @@ rospy.on_shutdown(shutdown)
 rate = rospy.Rate(20)
 
 # Set the parameters for the target square
-goal_distance = rospy.get_param("~goal_distance", 1.0) # meters
-goal_angle = rospy.get_param("~goal_angle", math.radians(90)) # degrees converted to radians
-linear_speed = rospy.get_param("~linear_speed", 0.2) # meters per second
-angular_speed = rospy.get_param("~angular_speed", 0.7) # radians per second
-angular_tolerance = rospy.get_param("~angular_tolerance", math.radians(2)) # degrees to radians
+linear_speed = rospy.get_param("~linear_speed", 50.0) # meters per second
 
 cmd_vel = rospy.Publisher('/robot_0/cmd_vel', geometry_msgs.msg.Twist, queue_size=1000)
 
 def bla(msg):
-	print msg
+	print(msg)
 
-my_bot = rospy.Subscriber('/robot_0/odom', nav_msgs.msg.Odometry, bla)
 
+#my_bot = rospy.Subscriber('/robot_0/base_pose_ground_truth', nav_msgs.msg.Odometry, bla)
+#other_bot_1 = rospy.Subscriber('/robot_0/cmd_vel', geometry_msgs.msg.Twist, bla)
+other_bot_1 = rospy.Subscriber('/robot_0/base_scan',sensor_msgs.msg.LaserScan, bla)
+
+"""This code has been commented out for now as it serves no purpose"""
 # The base frame is base_footprint for the TurtleBot but base_link for Pi Robot
 #base_frame = rospy.get_param('~base_frame', '/base_link')
 
