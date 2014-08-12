7967d131f90d753f2fcd855aa4588c4d88f8ab38
root <root@softeng306.vm.bhd-akl1.xsrv.net.nz>
2014-08-12 15:51:34 +1200

    make the node move

--- se306/src/package1/scripts/node1.py
+++ se306/src/package1/scripts/node1.py
@@ -4,6 +4,7 @@ import roslib
 import rospy
 import geometry_msgs.msg
 import math
+import tf
 
 def shutdown():
 	print "Shutting down node1.py."
@@ -21,4 +22,37 @@ linear_speed = rospy.get_param("~linear_speed", 0.2) # meters per second
 angular_speed = rospy.get_param("~angular_speed", 0.7) # radians per second
 angular_tolerance = rospy.get_param("~angular_tolerance", math.radians(2)) # degrees to radians
 
-cmd_vel = rospy.Publisher('/cmd_vel', geometry_msgs.msg.Twist, queue_size=1000)
+cmd_vel = rospy.Publisher('/robot_0/cmd_vel', geometry_msgs.msg.Twist, queue_size=1000)
+
+# The base frame is base_footprint for the TurtleBot but base_link for Pi Robot
+#base_frame = rospy.get_param('~base_frame', '/base_link')
+
+# The odom frame is usually just /odom
+#odom_frame = rospy.get_param('~odom_frame', '/odom')
+
+#listener = tf.TransformListener()
+
+# Set the odom frame
+#odom_frame = '/odom'
+
+# Find out if the robot uses /base_link or /base_footprint
+#try:
+#		listener.waitForTransform(odom_frame, '/base_footprint', rospy.Time(), rospy.Duration(1.0))
+#		base_frame = '/base_footprint'
+#except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+#	try:
+#		listener.waitForTransform(odom_frame, '/base_link', rospy.Time(), rospy.Duration(1.0))
+#		base_frame = '/base_link'
+#	except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+#		rospy.loginfo("Cannot find transform between /odom and /base_link or /base_footprint")
+#		rospy.signal_shutdown("tf Exception")
+
+while not rospy.is_shutdown():
+	try:
+		move_cmd = geometry_msgs.msg.Twist()
+		move_cmd.linear.x = linear_speed
+		cmd_vel.publish(move_cmd)
+		rate.sleep()
+	except rospy.exceptions.ROSInterruptException:
+		print "Quitting."
+		break
