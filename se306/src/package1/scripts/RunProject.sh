0a0b2d85a18513e86828a5bffb49500da04e6f2a
Ben Brown <benbrown93@gmail.com>
2014-08-13 21:33:11 +1200

    Updated script to run with the stopper_node as well

--- se306/src/package1/scripts/RunProject.sh
+++ se306/src/package1/scripts/RunProject.sh
@@ -3,5 +3,6 @@
 nohup xterm -e "source /306A1/se306/devel/setup.bash; roscore" >/dev/null 2>&1 & 
 nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun stage_ros stageros world/myworld.world" >/dev/null 2>&1 &
 nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 node1.py" >/dev/null 2>&1 &
+nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 stopper_node.py" >/dev/null 2>&1 &
 clear
 exit
