320a073cbbb8142e2b996dea560caed9f5ce4e50
swanndri <benbrown93@gmail.com>
2014-08-13 22:43:32 +1200

    Changed indentation to tabs in stopper

--- se306/src/package1/scripts/stopper_node.py
+++ se306/src/package1/scripts/stopper_node.py
@@ -16,71 +16,71 @@ from math import tanh
 
 
 class Stopper:
-    def __init__(self, distance, max_speed=1, min_speed=0.01):
-        # How close should we get to things, and what's our maximum speed?
-        self.distance = distance
-        self.max_speed = max_speed
-        self.min_speed = min_speed
+	def __init__(self, distance, max_speed=1, min_speed=0.01):
+		# How close should we get to things, and what's our maximum speed?
+		self.distance = distance
+		self.max_speed = max_speed
+		self.min_speed = min_speed
 
-        # Subscriber for the laser data
-        self.sub = rospy.Subscriber('/robot_2/base_scan', LaserScan, self.laser_callback)
+		# Subscriber for the laser data
+		self.sub = rospy.Subscriber('/robot_2/base_scan', LaserScan, self.laser_callback)
 
-        # Publisher for movement commands
-        self.pub = rospy.Publisher('/robot_2/cmd_vel', Twist)
+		# Publisher for movement commands
+		self.pub = rospy.Publisher('/robot_2/cmd_vel', Twist)
 
-        # Let the world know we're ready
-        rospy.loginfo('Stopper initialized')
+		# Let the world know we're ready
+		rospy.loginfo('Stopper initialized')
 
-    def laser_callback(self, scan):
-        # What's the closest laser reading
-        closest = min(scan.ranges)
-        
-        # This is the command we send to the robot
-        command = Twist()
+	def laser_callback(self, scan):
+		# What's the closest laser reading
+		closest = min(scan.ranges)
+		
+		# This is the command we send to the robot
+		command = Twist()
 
-        # If we're much more than 50cm away from things, then we want
-        # to be going as fast as we can.  Otherwise, we want to slow
-        # down.  A hyperbolic tangent transfer function will do this
-        # nicely
-        """.
-        command.linear.x = tanh(5 * (closest - self.distance)) * self.max_speed
-        command.linear.y = 0.0
-        command.linear.z = 0.0
-        command.angular.x = 0.0
-        command.angular.y = 0.0
-        command.angular.z = 0.0
-        """
-        #This was the previous code from script before -- 
-    #If we're going too slowly, then just stop
-        #if abs(command.linear.x) < self.min_speed:
-        #    command.linear.x = 0
-    
-        if closest < self.distance:
-            command.linear.x = -1
-        else:
-            command.linear.x = 1
+		# If we're much more than 50cm away from things, then we want
+		# to be going as fast as we can.  Otherwise, we want to slow
+		# down.  A hyperbolic tangent transfer function will do this
+		# nicely
+		""".
+		command.linear.x = tanh(5 * (closest - self.distance)) * self.max_speed
+		command.linear.y = 0.0
+		command.linear.z = 0.0
+		command.angular.x = 0.0
+		command.angular.y = 0.0
+		command.angular.z = 0.0
+		"""
+		#This was the previous code from script before -- 
+		#If we're going too slowly, then just stop
+		#if abs(command.linear.x) < self.min_speed:
+		#    command.linear.x = 0
+	
+		if closest < self.distance:
+			command.linear.x = -1
+		else:
+			command.linear.x = 1
 
-        command.linear.y = 0.0
-        command.linear.z = 0.0
-        command.angular.x = 0.0
-        command.angular.y = 0.0
-        command.angular.z = 0.0
+		command.linear.y = 0.0
+		command.linear.z = 0.0
+		command.angular.x = 0.0
+		command.angular.y = 0.0
+		command.angular.z = 0.0
 
-        rospy.logdebug('Distance: {0}, speed: {1}'.format(closest, command.linear.x))
+		rospy.logdebug('Distance: {0}, speed: {1}'.format(closest, command.linear.x))
 
-        # Send the command to the motors
-        self.pub.publish(command)
+		# Send the command to the motors
+		self.pub.publish(command)
 
 
 if __name__ == '__main__':
-    rospy.init_node('stopper')
+	rospy.init_node('stopper')
 
-    # Get the distance from the parameter server.  Default is 0.5
-    distance = rospy.get_param('distance', 0.5)
+	# Get the distance from the parameter server.  Default is 0.5
+	distance = rospy.get_param('distance', 0.5)
 
 
-    # Set up the controller
-    stopper = Stopper(distance)
+	# Set up the controller
+	stopper = Stopper(distance)
 
-    # Hand control over to ROS
-    rospy.spin()
+	# Hand control over to ROS
+	rospy.spin()
