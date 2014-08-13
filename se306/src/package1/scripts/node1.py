5bd0b7f940c8329cca7c55ade3972344b1c75420
root <root@softeng306.vm.bhd-akl1.xsrv.net.nz>
2014-08-13 13:42:56 +1200

    make robot reverse after hitting an object

--- se306/src/package1/scripts/node1.py
+++ se306/src/package1/scripts/node1.py
@@ -7,7 +7,7 @@ import std_msgs.msg
 import nav_msgs.msg
 import sensor_msgs.msg
 import math
-import tf
+import time
 
 def shutdown():
 	print "Shutting down node1.py."
@@ -16,72 +16,45 @@ roslib.load_manifest('package1')
 
 rospy.init_node('node1', anonymous=False)
 rospy.on_shutdown(shutdown)
-rate = rospy.Rate(20)
+rate = rospy.Rate(10)
 
-# Set the parameters for the target square
-linear_speed = rospy.get_param("~linear_speed", 50.0) # meters per second
+linear_speed = rospy.get_param("~linear_speed", 50.0)
 
-cmd_vel = rospy.Publisher('/robot_0/cmd_vel', geometry_msgs.msg.Twist, queue_size=1000)
-recq = []
+cmd_vel = rospy.Publisher('/robot_0/cmd_vel', geometry_msgs.msg.Twist, queue_size=10)
 
+buffer = []
 
-def bla(msg):
+def base_pose_ground_truth_received(msg):
+	print("Got bgpt message at %s" % time.time())
+	buffer.append(('bpgt', msg))
 
-	"""
-	if msg.twist.twist.linear.x == 0:
-		move_cmd = geometry_msgs.msg.Twist()
-		move_cmd.linear.x = linear_speed * -1
-		cmd_vel.publish(move_cmd)
-		"""
+def cmd_vel_received(msg):
+	print("Got cv message at %s" % time.time())
+	buffer.append(('cv', msg))
 
-	recq.append(msg)
-
-def bla2(msg):
-	print(msg)
-
-
-my_bot = rospy.Subscriber('/robot_0/base_pose_ground_truth', nav_msgs.msg.Odometry, bla)
-other_bot_1 = rospy.Subscriber('/robot_0/cmd_vel', geometry_msgs.msg.Twist, bla2)
-#other_bot_1 = rospy.Subscriber('/robot_0/base_scan',sensor_msgs.msg.LaserScan, bla)
-
-"""This code has been commented out for now as it serves no purpose"""
-# The base frame is base_footprint for the TurtleBot but base_link for Pi Robot
-#base_frame = rospy.get_param('~base_frame', '/base_link')
-
-# The odom frame is usually just /odom
-#odom_frame = rospy.get_param('~odom_frame', '/odom')
-
-#listener = tf.TransformListener()
-
-# Set the odom frame
-#odom_frame = '/odom'
-
-# Find out if the robot uses /base_link or /base_footprint
-#try:
-#		listener.waitForTransform(odom_frame, '/base_footprint', rospy.Time(), rospy.Duration(1.0))
-#		base_frame = '/base_footprint'
-#except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-#	try:
-#		listener.waitForTransform(odom_frame, '/base_link', rospy.Time(), rospy.Duration(1.0))
-#		base_frame = '/base_link'
-#	except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-#		rospy.loginfo("Cannot find transform between /odom and /base_link or /base_footprint")
-#		rospy.signal_shutdown("tf Exception")
+rospy.Subscriber('/robot_0/base_pose_ground_truth', nav_msgs.msg.Odometry, base_pose_ground_truth_received)
+#rospy.Subscriber('/robot_0/cmd_vel', geometry_msgs.msg.Twist, cmd_vel_received)
+#rospy.Subscriber('/robot_0/base_scan',sensor_msgs.msg.LaserScan, bla)
 
 while not rospy.is_shutdown():
 	try:
-		print(len(recq))
-		if len(recq)>0:
-			command = recq.pop(0)
-			print (command.twist.twist.linear.x)
-			if command.twist.twist.linear.x == 0:
+		if len(buffer) > 0:
+			print("We have %s messages to process." % len(buffer))
+			while buffer:
+				(message_type, message) = buffer.pop(0)
+				#print("Popped %s message (%d more in the queue)" % (message_type, len(buffer)))
+				
+				if message_type == 'bpgt':
+					# check if the robot is stuck
+					if message.twist.twist.linear.x == 0:
+						linear_speed *= -1
+				elif message_type == 'cv':
+					pass
+				
 				move_cmd = geometry_msgs.msg.Twist()
-				move_cmd.linear.x = linear_speed * -1
-		else:
-			move_cmd = geometry_msgs.msg.Twist()
-			move_cmd.linear.x = linear_speed
-	
-		cmd_vel.publish(move_cmd)
+				move_cmd.linear.x = linear_speed
+				cmd_vel.publish(move_cmd)
+		
 		rate.sleep()
 	except rospy.exceptions.ROSInterruptException:
 		print "Quitting."
