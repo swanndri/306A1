5597224606006c68fa83d8f8981a6ce4d7ca8a19
root <root@softeng306.vm.bhd-akl1.xsrv.net.nz>
2014-08-13 18:21:13 +1200

    pipe nohup to blackhole to avoid nohup files

--- se306/src/package1/scripts/RunProject.sh
+++ se306/src/package1/scripts/RunProject.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run 306 project easily
-nohup xterm -e "source /306A1/se306/devel/setup.bash; roscore" &
-nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun stage_ros stageros world/myworld.world" &
-nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 node1.py" &
+nohup xterm -e "source /306A1/se306/devel/setup.bash; roscore" >/dev/null 2>&1 & 
+nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun stage_ros stageros world/myworld.world" >/dev/null 2>&1 &
+nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 node1.py" >/dev/null 2>&1 &
 clear
 exit
