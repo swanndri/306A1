b0352e5194822b383e4e13343692d64e188ec517
root <root@softeng306.vm.bhd-akl1.xsrv.net.nz>
2014-08-13 18:17:37 +1200

    wrote dumb move algorithm

--- se306/src/package1/scripts/node1.py
+++ se306/src/package1/scripts/node1.py
@@ -8,6 +8,7 @@ import nav_msgs.msg
 import sensor_msgs.msg
 import math
 import time
+import random
 
 def shutdown():
 	print "Shutting down node1.py."
@@ -18,18 +19,23 @@ rospy.init_node('node1', anonymous=False)
 rospy.on_shutdown(shutdown)
 rate = rospy.Rate(10)
 
-linear_speed = rospy.get_param("~linear_speed", 50.0)
+rospy.set_param("~linear_speed_x", 1)
+rospy.set_param("~linear_speed_y", 1)
+
+debug_level = 1
 
 cmd_vel = rospy.Publisher('/robot_0/cmd_vel', geometry_msgs.msg.Twist, queue_size=10)
 
 buffer = []
 
 def base_pose_ground_truth_received(msg):
-	print("Got bgpt message at %s" % time.time())
+	if debug_level > 2:
+		print("Got bgpgt message at %s" % time.time())
 	buffer.append(('bpgt', msg))
 
 def cmd_vel_received(msg):
-	print("Got cv message at %s" % time.time())
+	if debug_level > 2:
+		print("Got cv message at %s" % time.time())
 	buffer.append(('cv', msg))
 
 rospy.Subscriber('/robot_0/base_pose_ground_truth', nav_msgs.msg.Odometry, base_pose_ground_truth_received)
@@ -39,20 +45,31 @@ rospy.Subscriber('/robot_0/base_pose_ground_truth', nav_msgs.msg.Odometry, base_
 while not rospy.is_shutdown():
 	try:
 		if len(buffer) > 0:
-			print("We have %s messages to process." % len(buffer))
+			if debug_level > 2:
+				print("We have %s messages to process." % len(buffer))
 			while buffer:
 				(message_type, message) = buffer.pop(0)
-				#print("Popped %s message (%d more in the queue)" % (message_type, len(buffer)))
+				if debug_level > 2:
+					print("Popped %s message (%d more in the queue)" % (message_type, len(buffer)))
 				
 				if message_type == 'bpgt':
 					# check if the robot is stuck
 					if message.twist.twist.linear.x == 0:
-						linear_speed *= -1
+						if debug_level > 0:
+								print("Robot hit an obstacle.")
+						rospy.set_param('~linear_speed_x', rospy.get_param('~linear_speed_x') * -1)
+						rospy.set_param('~angular_z', random.uniform(0, 2))
+						rospy.set_param('~angular_iterations', random.randint(0, 6))
 				elif message_type == 'cv':
 					pass
 				
 				move_cmd = geometry_msgs.msg.Twist()
-				move_cmd.linear.x = linear_speed
+				move_cmd.linear.x = rospy.get_param('~linear_speed_x')
+				move_cmd.linear.y = rospy.get_param('~linear_speed_y')
+				
+				if rospy.get_param('~angular_iterations') > 0:
+					move_cmd.angular.z = rospy.get_param('~angular_z')
+					rospy.set_param('~angular_iterations', rospy.get_param('~angular_iterations')-1)
 				cmd_vel.publish(move_cmd)
 		
 		rate.sleep()
