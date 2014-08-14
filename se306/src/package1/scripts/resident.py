9d442d205a2152d474d93fb87f9522867108cced
Patrick Free <patrickfree@outlook.com>
2014-08-15 01:05:55 +1200

    Subscribe resident to clock and implement publishing to status.py

--- se306/src/package1/scripts/resident.py
+++ se306/src/package1/scripts/resident.py
@@ -5,19 +5,41 @@ import roslib
 import rospy
 import rosgraph_msgs
 
-from std_msgs.msg import String
+import std_msgs.msg
 
-def process_event(action_msg):
-	#print('test')
-	message = str(action_msg).split("data: ")[1]
-	print(message)
-    
-def listener():
+class Resident:
+	
+	def __init__(self):
+		self.fullness = 100
+		
+	
+	def pub_sub(self):
+		sub = rospy.Subscriber("clock", std_msgs.msg.String, self.callback)
+		rospy.Subscriber("scheduler", std_msgs.msg.String, self.process_event)
+		self.pub = rospy.Publisher('human', std_msgs.msg.String, \
+		queue_size=10)
+		
+	def callback(self, msg):
+		
+		#pulls the secs value from the Clock object
+		if int(str(msg).split()[2]) % 14 == 0:
+			self.fullness -= 1
+			print self.fullness
+			self.pub.publish("Fullness: " + str(self.fullness))	
+		
+	
 
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("scheduler", String, process_event)
-
-    rospy.spin()
+	def process_event(self, action_msg):
+		#print('test')
+		message = str(action_msg).split("data: ")[1]
+		print(message)
 
 if __name__ == '__main__':
-    listener()
+	rospy.init_node('resident', anonymous=True)
+	
+	# Set up the controller
+	resident = Resident()
+	resident.pub_sub()
+
+    # Hand control over to ROS
+	rospy.spin()
