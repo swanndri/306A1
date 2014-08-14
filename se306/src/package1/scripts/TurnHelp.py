ebac1e807f39515a344fbbc89005fda55989e0f3
Jordan Hohepa <jhoh410@aucklanduni.ac.nz>
2014-08-15 01:09:57 +1200

    Added a TurnHelp.py script which will help robot turn the faster way, still needs to be changed to allow input from other scripts, output to robot and change rad to deg

---
+++ se306/src/package1/scripts/TurnHelp.py
@@ -0,0 +1,49 @@
+
+class Angles:
+
+	def check(angle_from, angle_to):
+		diff = angle_from-angle_to
+		if (angle_to >= 0) and (angle_to < 90): 
+			if (angle_from >= 0) and (angle_from <= 90):
+				if diff > 0:
+					print ("Anti-Clockwise")
+				else:
+					print ("Clockwise")
+			else:
+				if diff > 0:
+					print ("Clockwise")
+				else:
+					print ("Anti-Clockwise")
+		else:
+			if (angle_from >= 0) and (angle_from < 90):
+				if (angle_to >= 180) and (angle_to <= 270):
+					if diff < 0:
+						print ("Anti-Clockwise")
+					else:
+						print ("Clockwise")
+				else:
+					if diff > 0:
+						print ("Anti-Clockwise")
+					else:
+						print ("Clockwise")
+			else:
+				if diff <= 0:
+					print ("Clockwise")
+				else:
+					print ("Anti-Clockwise")
+
+	def normalize(angle_norm):
+		newAngle = int(angle_norm)
+    		if newAngle < 0:
+			newAngle += 360;
+		return newAngle
+
+	what = input("Enter Angle from: ")
+	huh = input("Enter Angle to: ")
+	what = normalize(what)
+	huh = normalize(huh)
+	check(what, huh)
+
+
+
+
