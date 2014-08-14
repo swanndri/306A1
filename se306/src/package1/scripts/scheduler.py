f4ed58b2acfeef61392202f6f5ebaf42a9fc772e
Simon du Preez <smdupnz@yahoo.co.nz>
2014-08-14 12:39:24 +1200

    Created time cycle for scheduler to simulate a day. -Simon du Preez contributer

--- se306/src/package1/scripts/scheduler.py
+++ se306/src/package1/scripts/scheduler.py
@@ -2,11 +2,22 @@
 
 import roslib
 import rospy
+import rosgraph_msgs
 
-def shutdown():
-	print "Shutting down scheduler.py."
+
+#time_publisher = rospy.Publisher('/clock', rosgraph_msgs.msg.Clock, queue_size=100)
+
+def reset_time_to_zero(time):
+	print(time.clock.secs % 300)
+		#t=rospy.Time(0)
+		#time_publisher.publish(t)
+
 
 roslib.load_manifest('package1')
 rospy.init_node('scheduler')
-rospy.on_shutdown(shutdown)
 
+
+rospy.Subscriber('/clock', rosgraph_msgs.msg.Clock, reset_time_to_zero)
+
+
+rospy.spin()
