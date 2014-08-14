ede2c74de3cb7128b388517fdfb65db9f2fedb64
Jak Tan <jtan325@aucklanduni.ac.nz>
2014-08-14 15:14:43 +1200

    Added events to the event scheduler and created a resident script to listen to the events - Contributers: Simon du Preez, Jak Tan

--- se306/src/package1/scripts/scheduler.py
+++ se306/src/package1/scripts/scheduler.py
@@ -11,24 +11,34 @@ from std_msgs.msg import String
 simulation_time = 0
 pub = rospy.Publisher('scheduler', String, queue_size=10)
 
+scheduled_tasks = { 480: 'Resident.wakeup', 
+				 	540: 'Resident.eat_breakfast',
+				 	660: 'Resident.take_meds',
+				 	750: 'Resident.eat_lunch',
+				 	1080:'Resident.eat_dinner',
+				 	1320:'Resident.sleep',
+
+				 	510: 'Cook.cook_breakfast',
+					720: 'Cook.cook_lunch',
+					1050:'Cook.cook_dinner'	}
+
 def publish(actionmsg):        
         pub.publish(actionmsg)
+        print(actionmsg)
 
-def wakeup():
-	actionmsg = 'wakeup'
-	publish(actionmsg)
 
 def schedule_events(time):
 	#1440 because there are 1440 minutes in a day therefore 1 minute simulation = 1 second real time
 	#0 = 12:00am (midnight). 
 	#480 = 8:00am
-
 	simulation_time  = time.clock.secs % 1440
+	actionmsg = None
 	if ( time.clock.nsecs == 100000000 ):
+		actionmsg = scheduled_tasks.get(simulation_time)
 		print(simulation_time)
-	if ( simulation_time == 480 and time.clock.nsecs == 100000000):
-		wakeup()
-		print('Sent wake up message')
+		
+		if (actionmsg is not None):
+			publish(actionmsg)
 		
 
 roslib.load_manifest('package1')
