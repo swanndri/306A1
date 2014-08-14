59411b67dd504e16367dd27b58ff9169f51ffb4e
Rahul Kumar <rkmu728@aucklanduni.ac.nz>
2014-08-15 02:23:22 +1200

    Updated RunProject.sh to run project with the movetest.py file instead of stopper.py

--- se306/src/package1/scripts/RunProject.sh
+++ se306/src/package1/scripts/RunProject.sh
@@ -3,6 +3,7 @@
 nohup xterm -e "source /306A1/se306/devel/setup.bash; roscore" >/dev/null 2>&1 & 
 nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun stage_ros stageros world/myworld.world; read" >/dev/null 2>&1 &
 #nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 node1.py; read" >/dev/null 2>&1 &
-nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 stopper_node.py; read" >/dev/null 2>&1 &
+#nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 stopper_node.py; read" >/dev/null 2>&1 &
+nohup xterm -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 movetest.py; read" >/dev/null 2>&1 &
 clear
 exit
