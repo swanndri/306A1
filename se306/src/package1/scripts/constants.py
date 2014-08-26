a8bcc0cba43f2f0fa66a5e5e9397564ff2513651
Milos Ivanovic <milos@ivanovic.co.nz>
2014-08-26 18:22:25 +1200

    tidy up constants.py: PEP8 said no extra spacing + moved comments to the right place

--- se306/src/package1/scripts/constants.py
+++ se306/src/package1/scripts/constants.py
@@ -1,15 +1,16 @@
 import math
 
-''' The Paths calls of constants is used in the Navigation class.
+class Paths(object):
 
-This class contains the invisible nodes we will use to make robots
-travel from room to room, aswell as paths that robots are likely to
-take e.g. door_to_kitchen.
+	""" The Paths calls of constants is used in the Navigation class.
 
-Directions have also been entered for north south east west as these
-are constants within our code
-'''
-class Paths:
+	This class contains the invisible nodes we will use to make robots
+	travel from room to room, aswell as paths that robots are likely to
+	take e.g. door_to_kitchen.
+
+	Directions have also been entered for north south east west as these
+	are constants within our code
+	"""
 
 	# Orientation constants
 	east = 0.0
@@ -18,19 +19,19 @@ class Paths:
 	south = -math.pi / 2.0
 
 	# Invisible nodes/points within our house
-	cupboard 		=	[-3.6, 4.2]
-	bedroom 		=	[-3.6, 2.1]
-	bathroom		=	[-3.6, -3.7]
-	hallway_top		=	[-1.2, 1.9]
-	hallway_mid		=	[-1.2, 0.45]
-	hallway_bot		=	[-1.2, -3.7]
-	door			=	[-1.2, -6]
-	kitchen			=	[2.05, 3.5]
+	cupboard = [-3.6, 4.2]
+	bedroom = [-3.6, 2.1]
+	bathroom = [-3.6, -3.7]
+	hallway_top = [-1.2, 1.9]
+	hallway_mid = [-1.2, 0.45]
+	hallway_bot = [-1.2, -3.7]
+	door = [-1.2, -6]
+	kitchen = 2.05, 3.5]
 	kitchen_entrance = [2.05, 1.6]
 	living_room_top_right = [4.5, 1.6]
-	living_room_middle	=	[2.05, 0.45]
-	cook_idle		= 	[4.5, -1.85]
-	idle			=	[12,4]
+	living_room_middle = [2.05, 0.45]
+	cook_idle = [4.5, -1.85]
+	idle = [12,4]
 
 	living_room_entrance = [1, 0.45]
 	living_room_sofa = [1, -3]
@@ -49,17 +50,21 @@ class Paths:
 
 
 	# Paths robots can take
-	door_to_kitchen				=	[door, hallway_mid, living_room_middle, kitchen]
-	bedroom_to_living_room		=	[bedroom, hallway_top, hallway_mid, living_room_entrance, living_room_sofa, sofa]
-	living_room_to_kitchen		=	[living_room_middle, kitchen]
-	kitchen_to_bedroom			=	[kitchen, living_room_middle, hallway_mid, hallway_top, bedroom, bed]
-	kitchen_to_cupboard			=	[kitchen, living_room_middle, hallway_mid, hallway_top, bedroom, cupboard, bedroom]
-	cupboard_to_kitchen			=	[bedroom, hallway_top, hallway_mid, living_room_middle, kitchen]
-	door_to_living_room			=	[door, hallway_mid, living_room_middle]
-	kitchen_to_sofa				=	[kitchen, living_room_middle, living_room_sofa, sofa]
-	cook_path					=	[cook_idle, living_room_top_right, kitchen_entrance, kitchen, kitchen_entrance, living_room_top_right, cook_idle]
+	door_to_kitchen = [door, hallway_mid, living_room_middle, kitchen]
+	bedroom_to_living_room = [bedroom, hallway_top, hallway_mid, living_room_entrance, living_room_sofa, sofa]
+	living_room_to_kitchen = [living_room_middle, kitchen]
+	kitchen_to_bedroom = [kitchen, living_room_middle, hallway_mid, hallway_top, bedroom, bed]
+	kitchen_to_cupboard = [kitchen, living_room_middle, hallway_mid, hallway_top, bedroom, cupboard, bedroom]
+	cupboard_to_kitchen = [bedroom, hallway_top, hallway_mid, living_room_middle, kitchen]
+	door_to_living_room = [door, hallway_mid, living_room_middle]
+	kitchen_to_sofa = [kitchen, living_room_middle, living_room_sofa, sofa]
+	cook_path = [cook_idle, living_room_top_right, kitchen_entrance, kitchen, kitchen_entrance, living_room_top_right, cook_idle]
 
 
-class RosConstants:
+class RosConstants(object):
+	""" ROS-specific constants, such as the rate at which the main loop will iterate. """
 
 	robot_rate = 10
+
+if __name__ == "__main__":
+	print "This was not intended to be run directly." 
