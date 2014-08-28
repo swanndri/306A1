1754c7219fe1dbe2a99b0bab70ec3de3de6411f8
Patrick Free <patrickfree@outlook.com>
2014-08-29 10:36:25 +1200

    Fixing naming of files.

--- se306/src/package1/scripts/RunProject.sh
+++ se306/src/package1/scripts/RunProject.sh
@@ -17,9 +17,9 @@ xterm -geometry 96x24-0+0 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se3
 
 # actual robots
 xterm -geometry 96x24-0-0 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 scheduler.py; read" > /dev/null 2>&1 &
-xterm -geometry 96x24+0-0 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 cook2.py; read" > /dev/null 2>&1 &
-xterm -geometry 96x24+100-100 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 visitor2.py; read" > /dev/null 2>&1 &
-xterm -geometry 96x24-100+100 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 resident2.py; read" > /dev/null 2>&1 &
+xterm -geometry 96x24+0-0 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 cook.py; read" > /dev/null 2>&1 &
+xterm -geometry 96x24+100-100 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 visitor.py; read" > /dev/null 2>&1 &
+xterm -geometry 96x24-100+100 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 resident.py; read" > /dev/null 2>&1 &
 xterm -geometry 96x24-200+100 -e "source /306A1/se306/devel/setup.bash;cd /306A1/se306/src/package1/;rosrun package1 status.py; read" > /dev/null 2>&1 &
 
 clear
