60fe174a6c2765b69cc6867b5aeb9bb77fe7204f
James Barwell-Smith <jbar894@aucklanduni.ac.nz>
2014-08-29 07:49:28 +1200

    nurse class created

--- se306/src/package1/scripts/nurse.py
+++ se306/src/package1/scripts/nurse.py
@@ -4,17 +4,17 @@ import rospy
 import node
 import database
 
-class Doctor(node.Robot):
+class Nurse(node.Robot):
 	"""
 
 	"""
 	def __init__(self, name):
-		super(Doctor, self).__init__(name)
+		super(Nurse, self).__init__(name)
 
 	def _scheduler_event_callback(self, msg):
 		if msg.data.split()[1].startswith(self.__class__.__name__):
 			self.jobs.put(tuple(msg.data.split()))
 
 if __name__ == '__main__':
-	rospy.init_node('robot_2')
-	doctor = Doctor('robot_2')
+	rospy.init_node('robot_3')
+	nurse = Nurse('robot_3')
