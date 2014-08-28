224aa8b36faabeb2434a96ac4e1d538d640a9e5b
James Barwell-Smith <jbar894@aucklanduni.ac.nz>
2014-08-29 08:40:06 +1200

    added entertainment robot

---
+++ se306/src/package1/scripts/entertain.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import rospy
+import node
+import database
+
+class Entertain(node.Robot):
+	"""
+
+	"""
+	def __init__(self, name):
+		super(Entertain, self).__init__(name)
+
+	def _scheduler_event_callback(self, msg):
+		if msg.data.split()[1].startswith(self.__class__.__name__):
+			self.jobs.put(tuple(msg.data.split()))
+
+if __name__ == '__main__':
+	rospy.init_node('robot_7')
+	entertain = Entertain('robot_7')
