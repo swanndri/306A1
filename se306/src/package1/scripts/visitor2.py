1c2b270ca3078f3cbda81ee855005ba30729de71
Milos Ivanovic <milos@ivanovic.co.nz>
2014-08-29 03:15:42 +1200

    clean up new workspace with help from: rahul and ben

--- se306/src/package1/scripts/visitor2.py
+++ se306/src/package1/scripts/visitor2.py
@@ -3,15 +3,15 @@
 import rospy
 import node
 
-class Cook(node.Robot):
+class Visitor(node.Robot):
 	
 	def __init__(self, name):
-		super(Cook, self).__init__(name)
+		super(Visitor, self).__init__(name)
 
 	def _scheduler_event_callback(self, msg):
 		if msg.data.split()[1].startswith(self.__class__.__name__):
 			self.jobs.put(tuple(msg.data.split()))
 
 if __name__ == '__main__':
-	rospy.init_node('robot_2')
-	cook = Cook('robot_2')
+	rospy.init_node('robot_0')
+	visitor = Visitor('robot_0')
