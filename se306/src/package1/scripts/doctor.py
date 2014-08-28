a080285460f2bf0c41f37ae6c18e295197c6a338
James Barwell-Smith <jbar894@aucklanduni.ac.nz>
2014-08-29 07:45:57 +1200

    doctor class

---
+++ se306/src/package1/scripts/doctor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import rospy
+import node
+import database
+
+class Doctor(node.Robot):
+	
+	def __init__(self, name):
+		super(Doctor, self).__init__(name)
+
+	def _scheduler_event_callback(self, msg):
+		if msg.data.split()[1].startswith(self.__class__.__name__):
+			self.jobs.put(tuple(msg.data.split()))
+
+if __name__ == '__main__':
+	rospy.init_node('robot_2')
+	doctor = Doctor('robot_2')
