24079e476905c41614754ff67be5a673a69143d3
James Barwell-Smith <jbar894@aucklanduni.ac.nz>
2014-08-29 08:33:56 +1200

    caregiver added

---
+++ se306/src/package1/scripts/caregiver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import rospy
+import node
+import database
+
+class CareGiver(node.Robot):
+	"""
+
+	"""
+	def __init__(self, name):
+		super(CareGiver, self).__init__(name)
+
+	def _scheduler_event_callback(self, msg):
+		if msg.data.split()[1].startswith(self.__class__.__name__):
+			self.jobs.put(tuple(msg.data.split()))
+
+if __name__ == '__main__':
+	rospy.init_node('robot_3')
+	careGiver = CareGiver('robot_3')
